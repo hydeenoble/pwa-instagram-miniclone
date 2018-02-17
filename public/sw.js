da155bf1518b0c651bebaee20f74e15507f69632
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-17 17:29:39 +0100

    reacting to items deleted from server

--- public/sw.js
+++ public/sw.js
@@ -77,10 +77,13 @@ self.addEventListener('fetch', function(event){
 			fetch(event.request)
 			.then(function(res){
 				var clonedRes = res.clone();
-				clonedRes.json()
+				clearAllData('posts')
+				.then(function(){
+					return clonedRes.json()
+				})
 				.then(function(data){
 					for (var key in data) {
-						writeData('posts', data[key])
+						writeData('posts', data[key]);
 					}
 				});
 				return res;
