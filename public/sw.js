115109339fd358c2cc9a038b3c28330a2e742ad9
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-17 12:28:21 +0100

    Optimising the cache only implementation

--- public/sw.js
+++ public/sw.js
@@ -1,27 +1,28 @@
-var CACHE_STATIC_NAME = 'static-v17';
+var CACHE_STATIC_NAME = 'static-v18';
 var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+	'/',
+	'/index.html',
+	'/offline.html',
+	'/src/js/app.js',
+	'/src/js/feed.js',
+	'/src/js/promise.js',
+	'/src/js/fetch.js',
+	'/src/js/material.min.js',
+	'/src/css/app.css',
+	'/src/css/feed.css',
+	'/src/images/main-image.jpg',
+	'https://fonts.googleapis.com/css?family=Roboto:400,700',
+	'https://fonts.googleapis.com/icon?family=Material+Icons',
+	'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+]
 
 self.addEventListener('install', function(event){
 	console.log('[Service Worker] Installing Service Worker ...', event);
 	event.waitUntil(
 		caches.open(CACHE_STATIC_NAME).then(function(cache){
 			console.log('[Service Worker] Precaching App shell');
-			cache.addAll([
-				'/',
-				'/index.html',
-				'/offline.html',
-				'/src/js/app.js',
-				'/src/js/feed.js',
-				'/src/js/promise.js',
-				'/src/js/fetch.js',
-				'/src/js/material.min.js',
-				'/src/css/app.css',
-				'/src/css/feed.css',
-				'/src/images/main-image.jpg',
-				'https://fonts.googleapis.com/css?family=Roboto:400,700',
-				'https://fonts.googleapis.com/icon?family=Material+Icons',
-				'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-			]);
+			cache.addAll([STATIC_FILES]);
 		})
 	);
 })
@@ -78,6 +79,16 @@ self.addEventListener('activate', function(event){
 // 	);
 // });
 
+function isInArray(string, array){
+	for(var i = 0; i < array.length; i++){
+		if (array[i] === string){
+			return true;
+		}
+	}
+
+	return false;
+}
+
 self.addEventListener('fetch', function(event){
 	var url = 'https://httpbin.org/get';
 
@@ -91,6 +102,10 @@ self.addEventListener('fetch', function(event){
 					return res;
 				});
 			})
+		);
+	}else if(isInArray(event.request.url, STATIC_FILES)){
+		event.respondWith(
+			caches.match(event.request)
 		);
 	}else{
 		event.respondWith(
