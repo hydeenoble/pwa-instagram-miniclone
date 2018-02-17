9925dc64f093bac79f876443539ff4fd84a908b7
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-17 12:06:32 +0100

    Cache then network fallback strategy implementation

--- public/sw.js
+++ public/sw.js
@@ -62,18 +62,58 @@ self.addEventListener('activate', function(event){
 // 	);
 // });
 
+// self.addEventListener('fetch', function(event){
+// 	event.respondWith(
+// 		fetch(event.request)
+// 		.then(function(res){
+// 			return caches.open(CACHE_DYNAMIC_NAME)
+// 			.then(function(cache){
+// 				cache.put(event.request.url, res.clone());
+// 				return res;
+// 			});
+// 		})
+// 		.catch(function(err){
+// 			return caches.match(event.request)
+// 		})
+// 	);
+// });
+
 self.addEventListener('fetch', function(event){
-	event.respondWith(
-		fetch(event.request)
-		.then(function(res){
-			return caches.open(CACHE_DYNAMIC_NAME)
+	var url = 'https://httpbin.org/get';
+
+	if(event.request.url.indexOf(url) > -1){
+		event.respondWith(
+			caches.open(CACHE_DYNAMIC_NAME)
 			.then(function(cache){
-				cache.put(event.request.url, res.clone());
-				return res;
-			});
-		})
-		.catch(function(err){
-			return caches.match(event.request)
-		})
-	);
+				return fetch(event.request)
+				.then(function(res){
+					cache.put(event.request, res.clone());
+					return res;
+				});
+			})
+		);
+	}else{
+		event.respondWith(
+			caches.match(event.request).then(function(response){
+				if(response){
+					return response;
+				}else{
+					return fetch(event.request)
+					.then(function(res){
+						caches.open(CACHE_DYNAMIC_NAME)
+						.then(function(cache){
+							cache.put(event.request.url, res.clone());
+							return res;
+						});
+					}).catch(function(err){
+						return caches.open(CACHE_STATIC_NAME)
+						.then(function(cache){
+							return cache.match('/offline.html');
+						});
+					});
+				}
+			})
+		);
+	}
+	
 });
