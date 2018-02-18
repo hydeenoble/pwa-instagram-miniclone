77c9478f5fb5a8e131c84175e94df68347d2039c
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-18 09:20:48 +0100

    Requesting Notification permission

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,4 +1,5 @@
 var deferredPrompt;
+var enableNotificationButtons = document.querySelectorAll('.enable-notifications');
 
 if(!window.Promise){
     window.Promise = Promise;
@@ -17,3 +18,21 @@ window.addEventListener('beforeinstallprompt', function(event){
     deferredPrompt = event;
     return false;
 });
+
+function askForNotificationPermission(){
+    Notification.requestPermission(function(result){
+        console.log('User Choice', result);
+        if(result !== 'granted'){
+            console.log('No notification permission granted!');
+        } else {
+
+        }
+    });
+}
+
+if('Notification' in window){
+    for (var i = 0; i < enableNotificationButtons.length; i++){
+        enableNotificationButtons[i].style.display = 'inline-block';
+        enableNotificationButtons[i],addEventListener('click', askForNotificationPermission);
+    }
+}
