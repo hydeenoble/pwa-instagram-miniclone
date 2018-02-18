3fd0989eefa49b55eb515a93e88d0b29cc267778
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-18 04:32:26 +0100

    Stoing post in indexedDB

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -118,7 +118,21 @@ form,addEventListener('submit', function(event){
   if('serviceWorker' in navigator && 'SyncManager' in window){
     navigator.serviceWorker.ready
     .then(function(sw){
-      sw.sync.register('sync-new-post');
+      var post = {
+        id: new Date().toISOString,
+        title: titleInput.value,
+        location: locationInput.value
+      }
+      writeData('sync-posts', post)
+      .then(function(){
+        return sw.sync.register('sync-new-post');
+      }).then(function() {
+        var snackbarContainer = document.querySelector('#confirmation-toast');
+        var data = { message: 'Your Post was saved for syncing'};
+        snackbarContainer.MaterialSnackbar.showSnackbar(data);
+      }).catch(function(err){
+        console.log(err);
+      });
     });
   }
 });
