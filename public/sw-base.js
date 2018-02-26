d8a9b753b36e06231fe9349bd6eac75670ee9aa9
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-26 14:44:39 +0100

    Initializing routing in workbox

--- public/sw-base.js
+++ public/sw-base.js
@@ -1,4 +1,10 @@
 importScripts('workbox-sw.prod.v2.1.2.js');
 
 const workboxSW = new self.WorkboxSW();
+
+workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/,
+    workboxSW.strategies.staleWhileRevalidate({
+        cacheName: 'google-fonts'
+    }));
+
 workboxSW.precache([]);
