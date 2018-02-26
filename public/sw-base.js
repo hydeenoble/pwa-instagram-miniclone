a5977ca358a76c47871dd797a6685af90b918341
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-26 14:49:11 +0100

    expanding dynamic caching

--- public/sw-base.js
+++ public/sw-base.js
@@ -7,4 +7,14 @@ workboxSW.router.registerRoute(/.*(?:googleapis|gstatic)\.com.*$/,
         cacheName: 'google-fonts'
     }));
 
+workboxSW.router.registerRoute('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+    workboxSW.strategies.staleWhileRevalidate({
+        cacheName: 'material-css'
+    }));
+
+workboxSW.router.registerRoute(/.*(?:firebasestorage\.googleapis|gstatic)\.com.*$/,
+    workboxSW.strategies.staleWhileRevalidate({
+        cacheName: 'post-images'
+    }));
+
 workboxSW.precache([]);
