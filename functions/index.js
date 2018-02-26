529ac646b66fea244df7b3d93ec21356c69e33e0
Emehinola Idowu <hydeenoble39@gmail.com>
2018-02-26 18:23:25 +0100

    handling background sync and push notification with workbox

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-var functions = require('firebase-functions');
+
+const functions = require('firebase-functions');
 var admin = require('firebase-admin');
 var cors = require('cors')({origin: true});
 var webpush = require('web-push');
@@ -10,12 +11,11 @@ var UUID = require('uuid-v4');
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-var serviceAccount = require("./service-key.json");
-
+var serviceAccount = require('./service-key.json');
 var gcconfig = {
     projectId: 'pwagram-e0ce6',
     keyFilename: 'service-key.json'
-};
+}
 
 var gcs = require('@google-cloud/storage')(gcconfig);
 
@@ -24,24 +24,25 @@ admin.initializeApp({
     databaseURL: 'https://pwagram-e0ce6.firebaseio.com/'
 });
 
-exports.storePostData = functions.https.onRequest(function (request, response) {
-    cors(request, response, function () {
+exports.storePostData = functions.https.onRequest(function (request, response){
+    cors(request, response, function() {
         var uuid = UUID();
         var formData = new formidable.IncomingForm();
-        formData.parse(request, function(err, fields, files) {
+        formData.parse(request, function(err, fields, files){
+            console.log('files', files);
+            console.log('fields',fields.id);
             fs.rename(files.file.path, '/tmp/' + files.file.name);
             var bucket = gcs.bucket('pwagram-e0ce6.appspot.com');
-
             bucket.upload('/tmp/' + files.file.name, {
                 uploadType: 'media',
                 metadata: {
                     metadata: {
                         contentType: files.file.type,
-                        firebaseStorageDownloadTokens: uuid
+                        firebaseStorageDownloadTokens: uuid,
                     }
                 }
-            }, function(err, file) {
-                if (!err) {
+            }, function(err, file){
+                if (!err){
                     admin.database().ref('posts').push({
                         id: fields.id,
                         title: fields.title,
@@ -50,37 +51,39 @@ exports.storePostData = functions.https.onRequest(function (request, response) {
                             lat: fields.rawLocationLat,
                             lng: fields.rawLocationLng
                         },
-                        image: 'https://firebasestorage.googleapis.com/v0/b/' + bucket.name + '/o/' + encodeURIComponent(file.name) + '?alt=media&token=' + uuid
-                    })
-                        .then(function () {
-                            webpush.setVapidDetails('mailto:business@academind.com', 'BI5YZulve60bzJ58w3KkRwxuChlFV_HjPaYeUftq6QKda0sWuNXlk7YhCkeoNjBQza3OCHRlMw6WEqOXYd5PFek', 'HiLuVcnBs-fnmVFPMhhlJCPjpt-LoEcB1RiIeNvVDYE');
-                            return admin.database().ref('subscriptions').once('value');
-                        })
-                        .then(function (subscriptions) {
-                            subscriptions.forEach(function (sub) {
-                                var pushConfig = {
-                                    endpoint: sub.val().endpoint,
-                                    keys: {
-                                        auth: sub.val().keys.auth,
-                                        p256dh: sub.val().keys.p256dh
-                                    }
-                                };
+                        image: 'https://firebasestorage.googleapis.com/v0/b/' +
+                        bucket.name + '/o/' + encodeURIComponent(file.name) +
+                        '?alt=media&token=' + uuid
 
-                                webpush.sendNotification(pushConfig, JSON.stringify({
-                                    title: 'New Post',
-                                    content: 'New Post added!',
-                                    openUrl: '/help'
-                                }))
-                                    .catch(function (err) {
-                                        console.log(err);
-                                    })
-                            });
-                            response.status(201).json({message: 'Data stored', id: fields.id});
-                        })
-                        .catch(function (err) {
-                            response.status(500).json({error: err});
+                    }).then(function(){
+                        webpush.setVapidDetails('mailto:hydeenoble39@gmail.com','BI5YZulve60bzJ58w3KkRwxuChlFV_HjPaYeUftq6QKda0sWuNXlk7YhCkeoNjBQza3OCHRlMw6WEqOXYd5PFek','HiLuVcnBs-fnmVFPMhhlJCPjpt-LoEcB1RiIeNvVDYE');
+
+                        return admin.database().ref('subscriptions').once('value');
+                    }).then(function(subscriptions){
+                        subscriptions.forEach(sub => {
+                            var pushConfig = {
+                                endpoint: sub.val().endpoint,
+                                keys: {
+                                    auth: sub.val().keys.auth,
+                                    p256dh: sub.val().keys.p256dh
+                                }
+                            }
+
+                            webpush.sendNotification(pushConfig, JSON.stringify({
+                                title: 'New Post!',
+                                content: 'New Post Added!',
+                                openUrl: '/help',
+                            }))
+                                .catch(function(err){
+                                    console.log(err);
+                                });
                         });
-                } else {
+                        response.status(201).json({message: 'Data stored', id: fields.id});
+                    }).catch(function(err){
+                        response.status(500).json({error: err});
+                    });
+
+                }else{
                     console.log(err);
                 }
             });
--- public/sw-base.js
+++ public/sw-base.js
@@ -67,3 +67,109 @@ workboxSW.router.registerRoute(function (routeData) {
 });
 
 workboxSW.precache([]);
+
+
+self.addEventListener('sync', function(event){
+    console.log('[Service Worker] Background syncing', event);
+    if(event.tag === 'sync-new-posts') {
+        console.log('[Service Worker] Syncing new Posts');
+        event.waitUntil(
+            readAllData('sync-posts')
+                .then(function(data){
+                    for (var dt of data){
+
+
+                        var postData = new FormData();
+                        postData.append('id', dt.id);
+                        postData.append('title', dt.title);
+                        postData.append('location', dt.location);
+                        postData.append('file', dt.picture, dt.id + '.png');
+                        postData.append('rawLocationLat', dt.rawLocation.lat);
+                        postData.append('rawLocationLng', dt.rawLocation.lng);
+
+                        fetch('https://us-central1-pwagram-e0ce6.cloudfunctions.net/storePostData', {
+                            method: 'POST',
+                            body: postData,
+                            mode: 'no-cors'
+                        })
+                            .then(function(res){
+                                console.log('Sent data', res);
+                                if (res.ok){
+                                    res.json()
+                                        .then(function(resData){
+                                            deleteSingleItem('sync-posts', resData.id);
+                                        })
+                                }
+                            })
+                            .catch(function(err){
+                                console.log('Error while sending data', err);
+                            });
+                    }
+                })
+                .catch(function(err){
+                    console.log('Error while fetching data from IDB', err);
+                })
+        );
+    }
+});
+
+self.addEventListener('notificationclick', function(event){
+    var notification = event.notification;
+    var action = event.action;
+
+    console.log(notification);
+
+    if(action === 'confirm'){
+        console.log('Confirm was chosen');
+        notification.close();
+    } else {
+        console.log(action);
+        event.waitUntil(
+            clients.matchAll()
+                .then(function(clis){
+                    var client = clis.find(function(c){
+                        return c.visibilityState === 'visible';
+                    });
+
+                    if(client !== undefined){
+                        client.navigate(notification.data.url);
+                        client.focus();
+                    } else {
+                        clients.openWindow(notification.data.url);
+                    }
+                })
+        );
+        notification.close();
+    }
+});
+
+self.addEventListener('notificationclosed', function(event){
+    console.log('Notification was closed', event);
+});
+
+self.addEventListener('push', function(event) {
+    console.log('Push Notification recieved', event);
+
+    var data = {
+        title: 'New!',
+        content: 'Something default happened',
+        openUrl: '/'
+    };
+
+    if(event.data){
+        data = JSON.parse(event.data.text());
+    }
+
+    var options = {
+        body: data.content,
+        icon: '/src/images/icons/app-icon-96x96.png',
+        badge: '/src/images/icons/app-icon-96x96.png',
+        data: {
+            url: data.openUrl
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
